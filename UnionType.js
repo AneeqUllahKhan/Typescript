781cb1e94976351ff86dae6cd2194123d2e5c53d
Aneeq Ullah Khan <aneequllah.khan@outlook.com>
2023-01-27 23:52:20 +0500

    TS Update

---
+++ UnionType.js
@@ -0,0 +1,8 @@
+// Union Type
+var f = "Aneeq";
+f = 30;
+console.log(f);
+// Union Type Mulitiple 
+var g = "Aneeq";
+g = true;
+console.log(g);
---
+++ UnionType.ts
@@ -0,0 +1,10 @@
+// Union Type
+
+let f:string|number = "Aneeq"
+f = 30
+console.log(f)
+
+// Union Type Mulitiple 
+let g:string|number|boolean = "Aneeq"
+g = true
+console.log(g)
